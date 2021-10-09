ba17f1a18d730b4fa4bb4b0aa36397fe7573b1a1
Siim Meerits <sh0@yutani.ee>
2021-10-09 18:04:20 +0300

    lcms/all: Use custom CMake script for compilation.

--- recipes/lcms/all/conanfile.py
+++ recipes/lcms/all/conanfile.py
@@ -1,7 +1,6 @@
-from conans import ConanFile, tools, AutoToolsBuildEnvironment, MSBuild
+from conans import ConanFile, tools, CMake
 from conans.tools import Version
 import os
-import shutil
 
 required_conan_version = ">=1.33.0"
 
@@ -22,16 +21,18 @@ class LcmsConan(ConanFile):
         "shared": False,
         "fPIC": True,
     }
+    generators = "cmake", "cmake_find_package"
+    exports_sources = ["CMakeLists.txt"]
 
-    _autotools = None
+    _cmake = None
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
 
     @property
-    def _settings_build(self):
-        return getattr(self, "settings_build", self.settings)
+    def _build_subfolder(self):
+        return "build_subfolder"
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -43,12 +44,6 @@ class LcmsConan(ConanFile):
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
-    def build_requirements(self):
-        if self.settings.compiler != "Visual Studio":
-            self.build_requires("gnu-config/cci.20201022")
-            if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
-                self.build_requires("msys2/cci.latest")
-
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
                   destination=self._source_subfolder, strip_root=True)
@@ -58,94 +53,26 @@ class LcmsConan(ConanFile):
             # since VS2015 vsnprintf is built-in
             path = os.path.join(self._source_subfolder, "src", "lcms2_internal.h")
             tools.replace_in_file(path, "#       define vsnprintf  _vsnprintf", "")
-        if self.settings.os == "Android" and tools.os_info.is_windows:
-            # remove escape for quotation marks, to make ndk on windows happy
-            tools.replace_in_file(os.path.join(self._source_subfolder, "configure"),
-                                  "s/[	 `~#$^&*(){}\\\\|;'\\\''\"<>?]/\\\\&/g",
-                                  "s/[	 `~#$^&*(){}\\\\|;<>?]/\\\\&/g")
 
-    def _build_visual_studio(self):
-        if tools.Version(self.version) <= "2.11":
-            vc_sln_subdir = "VC2013"
-        else:
-            vc_sln_subdir = "VC2015"
-        with tools.chdir(os.path.join(self._source_subfolder, "Projects", vc_sln_subdir )):
-            target = "lcms2_DLL" if self.options.shared else "lcms2_static"
-            upgrade_project = Version(self.settings.compiler.version) > "12"
-            # run build
-            msbuild = MSBuild(self)
-            msbuild.build("lcms2.sln", targets=[target], platforms={"x86": "Win32"}, upgrade_project=upgrade_project)
-
-    def _configure_autotools(self):
-        if self._autotools:
-            return self._autotools
-        self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
-        yes_no = lambda v: "yes" if v else "no"
-        args = [
-            "--enable-shared={}".format(yes_no(self.options.shared)),
-            "--enable-static={}".format(yes_no(not self.options.shared)),
-            "--without-tiff",
-            "--without-jpeg",
-        ]
-        self._autotools.configure(args=args, configure_dir=self._source_subfolder)
-        return self._autotools
-
-    @property
-    def _user_info_build(self):
-        # If using the experimental feature with different context for host and
-        # build, the 'user_info' attributes of the 'build_requires' packages
-        # will be located into the 'user_info_build' object. In other cases they
-        # will be located into the 'deps_user_info' object.
-        return getattr(self, "user_info_build", None) or self.deps_user_info
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
 
     def build(self):
         self._patch_sources()
-        if self.settings.compiler == "Visual Studio":
-            self._build_visual_studio()
-        else:
-            shutil.copy(self._user_info_build["gnu-config"].CONFIG_SUB,
-                        os.path.join(self._source_subfolder, "config.sub"))
-            shutil.copy(self._user_info_build["gnu-config"].CONFIG_GUESS,
-                        os.path.join(self._source_subfolder, "config.guess"))
-            autotools = self._configure_autotools()
-            autotools.make()
+        cmake = self._configure_cmake()
+        cmake.build()
 
     def package(self):
         self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
-        if self.settings.compiler == "Visual Studio":
-            self.copy(pattern="*.h", src=os.path.join(self._source_subfolder, "include"), dst="include", keep_path=True)
-            if self.options.shared:
-                self.copy(pattern="*.lib", src=os.path.join(self._source_subfolder, "bin"), dst="lib", keep_path=False)
-                self.copy(pattern="*.dll", src=os.path.join(self._source_subfolder, "bin"), dst="bin", keep_path=False)
-            else:
-                self.copy(pattern="*.lib", src=os.path.join(self._source_subfolder, "Lib", "MS"), dst="lib",
-                          keep_path=False)
-        else:
-            autotools = self._configure_autotools()
-            autotools.install()
-            # remove entire share directory
-            tools.rmdir(os.path.join(self.package_folder, "share"))
-            # remove pkgconfig
-            tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
-            # remove la files
-            la = os.path.join(self.package_folder, "lib", "liblcms2.la")
-            if os.path.isfile(la):
-                os.unlink(la)
-            # remove binaries
-            for bin_program in ["tificc", "linkicc", "transicc", "psicc", "jpgicc"]:
-                for ext in ["", ".exe"]:
-                    try:
-                        os.remove(os.path.join(self.package_folder, "bin", bin_program + ext))
-                    except:
-                        pass
+        cmake = self._configure_cmake()
+        cmake.install()
 
     def package_info(self):
-        if self.settings.compiler == "Visual Studio":
-            self.cpp_info.libs = ["lcms2" if self.options.shared else "lcms2_static"]
-            if self.options.shared:
-                self.cpp_info.defines.append("CMS_DLL")
-        else:
-            self.cpp_info.libs = ["lcms2"]
         self.cpp_info.names["pkg_config"] = "lcms2"
-        if self.settings.os in ("FreeBSD", "Linux"):
+        self.cpp_info.libs = ["lcms2"]
+        if self.settings.os == "Linux":
             self.cpp_info.system_libs.append("m")
